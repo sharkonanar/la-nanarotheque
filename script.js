c8ed340d8896f2accd45b351b56b916511a49ee2
sharkonanar <carrygerard01@gmail.com>
2025-12-30 15:20:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.addEventListener('load', function() {
     const grille = document.getElementById('video-grid');
     
-    // Vérifie que le tableau 'videos' existe bien
     if (typeof videos !== 'undefined') {
         grille.innerHTML = ""; 
 
@@ -9,13 +8,19 @@ window.addEventListener('load', function() {
             grille.innerHTML += `
                 <div class="vhs-case">
                     <a href="video.html?id=${film.id}" class="vhs-link">
+                        
+                        <div class="vhs-front">
+                            <img src="images/jaquette.jpg" class="box-blank">
+                            <div class="poster-overlay" style="background-image: url('images/posters/${film.id}.jpg')"></div>
+                        </div>
+
                         <div class="vhs-spine">
                             <img src="images/vhs-texture.png" class="texture-overlay">
-                            
                             <div class="vhs-info">
                                 <span class="vhs-title">${film.titre}</span>
                             </div>
                         </div>
+
                     </a>
                 </div>
             `;
